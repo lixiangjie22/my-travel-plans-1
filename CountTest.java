312ae44b7594c6f540b7f2efc94f9867d1204691
李相杰 <571720581@qq.com>
2020-06-08 00:41:16 +0800

    test5 code

---
+++ CountTest.java
@@ -0,0 +1,29 @@
+package sizeyunsuan;
+
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCount() {
+		Stack<String> z = new Stack<String>();
+		int num1=5,num2=7,d;
+		z.push(String.valueOf(12));
+	}
+
+}
+
